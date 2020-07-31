fc76a2456b3e03b1d9d8bd0537ac37fb3b290b4e
holmen1 <holmen1@gmail.com>
2020-07-31 11:56:16 +0200

    Read file into word sequence

---
+++ spelling-corrector/SpellingCorrector/kladd.fsx
@@ -0,0 +1,13 @@
+open System.IO
+open System.Text.RegularExpressions
+
+let words str =
+    let rx = Regex @"\w+"
+    rx.Matches str
+
+let alltext = File.ReadAllText(@"../Data/micro.txt")
+let matches = words alltext
+let matchToWord (wordMatch:Match) = wordMatch.Value
+let s = Seq.map matchToWord (Seq.cast matches)
+// val it : seq<string> = seq ["The"; "Project"; "Gutenberg"; "EBook"; ...]
+
