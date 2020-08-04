1d209e8986424a54226244cf752346c2fe1824a1
holmen1 <holmen1@gmail.com>
2020-08-04 18:55:55 +0200

    Add word counter

--- spelling-corrector/SpellingCorrector/Program.fs
+++ spelling-corrector/SpellingCorrector/Program.fs
@@ -18,7 +18,15 @@ module SpellingCorrector =
     let path = @"../Data/micro.txt"
 
     let lower (str:string) =
-        str.ToLower() 
+        str.ToLower()
+
+    // tally occurrences of words in a sequence
+    // [("am", 2); ("12", 1); ("a", 62); ...]
+    let wordmap words =
+        let count = (fun word -> Seq.filter (fun w -> w = word) words |> Seq.length)
+        Seq.distinct words
+        |> Seq.map (fun w -> (w, count w))
+        |> Map.ofSeq
 
 
 [<EntryPoint>]
@@ -26,7 +34,10 @@ let main argv =
     for str in argv do
         let words = SpellingCorrector.words str
         let lowered = Seq.map SpellingCorrector.lower words
+        let WORDS = SpellingCorrector.wordmap lowered
         printfn "Words in '%s' is:\n" str
         for w in lowered do
             printfn "- %s\n" w
+        Seq.iter (fun item -> printfn "%O" item) WORDS
+        
     0
--- spelling-corrector/SpellingCorrector/kladd.fsx
+++ spelling-corrector/SpellingCorrector/kladd.fsx
@@ -5,10 +5,44 @@ let words str =
     let rx = Regex @"\w+"
     rx.Matches str
 
-let alltext = File.ReadAllText(@"../Data/micro.txt")
+let alltext = File.ReadAllText(@"../Data/small.txt")
 let matches = words alltext
 let matchToWord (wordMatch:Match) = wordMatch.Value
 let s = Seq.map matchToWord (Seq.cast matches)
 // val it : seq<string> = seq ["The"; "Project"; "Gutenberg"; "EBook"; ...]
 
 let lower = (fun (str:string) -> str.ToLower()) 
+
+let lowerSeq = Seq.map lower s
+
+
+let l = ["a";"bad";"bad";"idea";"bad";"idea"]
+List.filter (fun x -> x = "bad") l |> List.length
+
+let countwords l word =
+    List.filter (fun w -> w = word) l
+    |> List.length
+
+let count w =
+    countwords 
+
+let dict l =
+    let count = countwords l
+    List.distinct l
+    |> List.map (fun w -> (w, count w))
+    |> Map.ofList
+
+
+let tst = seq { "i"; "am"; "a"; "a"; "and"; "i"; "am"; "also"; "a"; "sequence" } 
+let testU = Seq.distinct tst
+
+let wordmap words =
+    let count = (fun word -> Seq.filter (fun w -> w = word) words |> Seq.length)
+    Seq.distinct words
+    |> Seq.map (fun w -> (w, count w))
+    |> Map.ofSeq
+
+// > wordmap tst;;                                                                    
+// val it : Map<string,int> =
+//   map
+//     [("a", 3); ("also", 1); ("am", 2); ("and", 1); ("i", 2); ("sequence", 1)]
